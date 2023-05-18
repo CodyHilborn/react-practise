f669eeeb7467eb621e3ef41dcf1209826ff0c897
Cody Hilborn <codyhilborn11@gmail.com>
2023-05-18 13:53:06 -0400

    Move expense date into seperate component, add styling

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseItemDate from "./ExpenseItemDate";
 
 const ExpenseItem = (props) => {
-  const { title, amount } = props;
-
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const { title, amount, date } = props;
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseItemDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
