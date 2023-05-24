d643eb7ed05bcd41ca9b96de8f009f239f5cb0db
Cody Hilborn <codyhilborn11@gmail.com>
2023-05-24 14:03:24 -0400

    Reconfigure folder structure

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseItemDate from "./ExpenseItemDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   const { title, amount, date } = props;
